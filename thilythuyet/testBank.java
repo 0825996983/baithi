2b239342a8e9b0c167a802db1add6a66480d492d
0825996983 <doquangbien3172003@gmail.com>
2023-03-03 09:23:05 +0700

    thijava

---
+++ thilythuyet/testBank.java
@@ -0,0 +1,9 @@
+package thilythuyet;
+
+public class testBank {
+    public static void main(String[] args) {
+     Bank b = new Bank(1000,30);
+        System.out.println("tien lai la :"+ b.annualInterestRate());
+
+    }
+}
---
+++ thilythuyet/testFlight.java
@@ -0,0 +1,9 @@
+package thilythuyet;
+public class testFlight {
+    public static void main(String[] args) {
+        Flight fl = new Flight(241, "Bac Ninh");
+        System.out.println(fl.getNumber());
+        System.out.println(fl.getDestination());
+        fl.display();
+    }
+}
